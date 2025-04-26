4b68361ecfaec20c7e8e341dbaee533ba211922e
Gabhideen <gabrielmotta.send@gmail.com>
2025-04-26 16:39:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-(function () {
-  // lista de domínios autorizados (temporário)
-  var _a = ["exemplo.com", "localhost"];
-  var _h = window.location.hostname.replace(/^www\./, "");
-  if (_a.indexOf(_h) < 0) {
-    // quebra total do site
+;(function(){
+-  var _a = ["exemplo.com","localhost"];
++  var _a = ["exemplo.com","localhost","gabhideen.github.io"];
+  var _h = window.location.hostname.replace(/^www\./,"");
+  if(_a.indexOf(_h) < 0){
     document.documentElement.innerHTML = "";
     document.documentElement.style.background = "#fff";
     window.stop();
